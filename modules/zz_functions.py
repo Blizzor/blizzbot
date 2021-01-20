963128af5b8b0c466ce6f5059ea935efb44d549e
Blizzor <Admin@Blizzor.de>
2021-01-20 13:37:57 +0100

    removed Log "Verbindung zur DB vorhanden"

--- modules/zz_functions.py
+++ modules/zz_functions.py
@@ -494,9 +494,7 @@ async def blacklist():
 
 async def dbcommit(sqlcommand, value = None):
     mydb = zz_init.getdb()
-    if(mydb.is_connected()):
-        print("Verbindung zur DB vorhanden")
-    else:
+    if(not mydb.is_connected()):
         print("Verbindung zur DB verloren...wird reconnected")
         mydb.reconnect(attempts=3, delay=5)
         mydb = zz_init.getdb()
